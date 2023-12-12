b0ee706d44f700900432ee96052b4b2f1c4bdf1a
David THIBAU <david.thibau@gmail.com>
2023-12-12 11:58:50 +0100

    Config backend + Invoke Service

--- 5_Integration/javascript/frontend/app.js
+++ 5_Integration/javascript/frontend/app.js
@@ -3,9 +3,11 @@ var app = express();
 var stringReplace = require('string-replace-middleware');
 
 var KC_URL = process.env.KC_URL || "http://localhost:8080";
+var SERVICE_URL = process.env.SERVICE_URL || "http://localhost:9000/protected";
 
 app.use(stringReplace({
-   'KC_URL': KC_URL
+   'KC_URL': KC_URL,
+   'SERVICE_URL': SERVICE_URL
 }));
 app.use(express.static('.'))
 
